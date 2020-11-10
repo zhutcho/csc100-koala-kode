c44b586e3183fe5859b53cdbfe0dc437d660dc81
Zane Hutchison <zanehutcho99@gmail.com>
2020-11-10 14:14:44 +1000

    Reordered the folders to clean everything up. Should make relative importing easier.

--- GUI/ReportPage.py
+++ GUI/ReportPage.py
@@ -1,36 +1,24 @@
-import GUI as gui
+import tkinter as tk
+from Database.CSC100DB import CSC100DB
 
 
-class ReportPage(gui.tk.Frame):
+class ReportPage(tk.Frame):
 
     def __init__(self, parent, controller):
         # self = this frame specific.
-        gui.tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
         # set controller to MainApp()
         self.controller = controller
 
-        # --- Change Anything to the GUI bellow here ---
-        """==============================================================="""
-
-        # Set Background Colour
-        self.config(bg="#7f8694")
-
-        # Return To Admin Page Button
-        return_button = gui.tk.Button(self, text="Return To Profile", relief=gui.tk.FLAT,
-                                      bg="#7f8694", fg="#023e8a", activebackground='#7f8694',
-                                      command=lambda: controller.show_frame(gui.ap.AdminPage), borderwidth=0,
-                                      activeforeground="#C5C8CF",
-                                      font="Helvetica 10"
-                                      )
-        return_button.place(relx=0.4, rely=0.70, anchor=gui.tk.W)
-
         # print to PDF Button
-        pdf_button = gui.tk.Button(self, text="Print to PDF", relief=gui.tk.FLAT,
-                                   bg="#7f8694", fg="#023e8a", activebackground='#7f8694',
-                                   command=lambda: gui.clicked(), borderwidth=0,
-                                   activeforeground="#C5C8CF",
-                                   font="Helvetica 10"
-                                   )
-        pdf_button.place(relx=0.6, rely=0.70, anchor=gui.tk.W)
+        pdf_button = tk.Button(self, text="Create PDF",
+                               relief=tk.FLAT, command=lambda: createPDF())
 
         # TODO Add Visual Graphs & Plan Layout
+
+
+# Function used for debugging purposes.
+
+
+def createPDF():
+    print(CSC100DB.callMonthlyProc())
