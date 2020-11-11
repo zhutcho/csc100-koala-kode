b0fa04ae3aedfd4e0f4b0acbcd3f16a3e28ef196
TheMiIkMan <71918699+TheMiIkMan@users.noreply.github.com>
2020-11-11 13:59:38 +1000

    Update ReportPage.py

--- GUI/ReportPage.py
+++ GUI/ReportPage.py
@@ -16,8 +16,38 @@ class ReportPage(tk.Frame):
         # print to PDF Button
         pdf_button = tk.Button(self, text="Create PDF",
                                relief=tk.FLAT, command=lambda: self.buttonClick())
+        pdf_button.place(relx=1, rely=0.57, anchor=tk.W)
         pdf_button.pack()
 
+        # drop down box
+
+        # Function used for drop down box
+        def show():
+            myLabel = tk.Label(self, text = clicked.get()).pack()
+
+        options = [
+            "Select Month",
+            "Janurary",
+            "Feburary",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        ]
+
+        clicked = tk.StringVar()
+        clicked.set(options[0])
+
+        drop = tk.OptionMenu(self, clicked, *options)
+        drop.pack()
+
+        dropButton = tk.Button(self, text = "Show Selection", command = show).pack()
         # TODO Add Visual Graphs & Plan Layout
 
 
