f362e2ac60a3709a08970748284bd352cc586b8e
Zane Hutchison <zanehutcho99@gmail.com>
2020-11-15 15:01:50 +1000

    Imports work again!

--- app/TestCreatePDF.py
+++ app/TestCreatePDF.py
@@ -10,9 +10,9 @@ print(pdf.getKeys(pdf.getDB().getMonthlyDataForTaxons('01', 2018)))
 print(pdf.getValues(pdf.getDB().getMonthlyDataForTaxons('01', 2018)))
 
 lga_bar_chart = pdf.getLGABarChart('01', 2018)
-pdf.drawingToPDF(lga_bar_chart, 'tests/test_lgabc.pdf')
+pdf.drawingToPDF(lga_bar_chart, 'app/tests/test_lgabc.pdf')
 
 taxons_bar_chart = pdf.getTaxonsBarChart('01', 2018)
-pdf.drawingToPDF(taxons_bar_chart, 'tests/test_taxonsbc.pdf')
+pdf.drawingToPDF(taxons_bar_chart, 'app/tests/test_taxonsbc.pdf')
 
-pdf.getMonthlyReport('tests/test_monthlyreport.pdf', '01', 2018)
+pdf.getMonthlyReport('app/tests/test_monthlyreport.pdf', '01', 2018)
