f6ca9ad19535eddcaac7b69e1181f674c77e03e7
TheMiIkMan <71918699+TheMiIkMan@users.noreply.github.com>
2020-11-15 15:56:38 +1000

    Update ReportPage.py

--- app/ReportPage.py
+++ app/ReportPage.py
@@ -1,6 +1,10 @@
 import tkinter as tk
-from CreatePDF import CreatePDF
-import datetime
+from database.CSC100DB import CSC100DB
+from PIL import Image,ImageTk
+from pdf2image import convert_from_path
+
+
+db = CSC100DB()
 
 
 class ReportPage(tk.Frame):
@@ -11,11 +15,66 @@ class ReportPage(tk.Frame):
         # set controller to MainApp()
         self.controller = controller
 
+        """report_frame = tk.Frame(self, width=480, height=240)
+        report_frame.pack()"""
+
         # Generates monthly report
 
         button1 = tk.Button(self, text="Generate monthly report",
-                               relief=tk.FLAT, command=lambda: pdf.getMonthlyReport(month, year)).pack()
+                               relief=tk.FLAT, command=lambda: self.buttonClick()).pack()
+        
+        # Adding Scrollbar to the PDF frame
+        scrol_y = tk.Scrollbar(self, orient=tk.VERTICAL)
+        # Adding text widget for inserting images
+        pdf = tk.Text(self, yscrollcommand=scrol_y.set,bg="grey")
+        # Setting the scrollbar to the right side
+        scrol_y.pack(side=tk.RIGHT,fill=tk.Y)
+        scrol_y.config(command=pdf.yview)
+        # Finally packing the text widget
+        pdf.pack()
+        # Here the PDF is converted to list of images
+        pages = convert_from_path('D:\Downloads\combinepdf.pdf',size=(800,900))
+        # Empty list for storing images
+        photos = []
+        # Storing the converted images into list
+        for i in range(len(pages)):
+          photos.append(ImageTk.PhotoImage(pages[i]))
+          
+        # Adding all the images to the text widget
+        for photo in photos:
+          pdf.image_create(tk.END,image=photo)
+        # For Seperating the pages
+          pdf.insert(tk.END,'\n\n')
 
         # TODO Add Visual Graphs & Plan Layout
 
 
+# Function used to generate monthly report
+
+    def buttonClick(self):
+        """print(db.getMonthlyDataForTaxons('01', 2018))
+        print(db.getMonthlyDataForLGA('01', 2018))
+        print(db.previousMonths('', '', ''))"""
+
+ 
+"""# Adding Scrollbar to the PDF frame
+scrol_y = tk.Scrollbar(orient=tk.VERTICAL)
+# Adding text widget for inserting images
+pdf = tk.Text(yscrollcommand=scrol_y.set,bg="grey")
+# Setting the scrollbar to the right side
+scrol_y.pack(side=tk.RIGHT,fill=tk.Y)
+scrol_y.config(command=pdf.yview)
+# Finally packing the text widget
+pdf.pack(fill=tk.BOTH,expand=1)
+# Here the PDF is converted to list of images
+pages = convert_from_path('D:\Downloads\combinepdf.pdf',size=(800,900))
+# Empty list for storing images
+photos = []
+# Storing the converted images into list
+for i in range(len(pages)):
+  photos.append(ImageTk.PhotoImage(pages[i]))
+# Adding all the images to the text widget
+for photo in photos:
+  pdf.image_create(tk.END,image=photo)
+# For Seperating the pages
+pdf.insert(tk.END,'\n\n')"""
